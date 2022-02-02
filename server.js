ebd502e5c20beb9f8fa62f4635473a89ed82fe6d
6y6exa <sokolovdenisost@gmail.com>
2022-02-02 16:12:44 +0300

    change jsonserver and react static

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+// запуск json-server
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ static: "build" });
-const port = process.env.PORT || 3000;
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => console.log("Server starting"));
+
+// запуск react static
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log("Server is starting in 3002 port...");
+});
