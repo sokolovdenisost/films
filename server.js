f5736dd161d9a33be33d2a41fdcbc1ac836c9992
6y6exa <sokolovdenisost@gmail.com>
2022-02-02 20:09:48 +0300

    1 try

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 3001;
 server.use(middlewares);
 server.use(router);
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 server.listen(PORT, () => {
   console.log("Server is running");
 });
