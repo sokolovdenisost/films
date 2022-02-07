49c952a01cc4fc3a2c760f005002a94193b8b4d5
6y6exa <sokolovdenisost@gmail.com>
2022-02-07 14:33:03 +0300

    add redux-sage and fix card.jsx, header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { Link, useLocation, useParams, useRoutes } from "react-router-dom";
 import { toggleAddModal } from "../../store/actions/mainAction";
 import { Button } from "../Button/Button";
 import { ModalAdd } from "../Modal/ModalAdd/ModalAdd";
@@ -9,6 +10,7 @@ import "./Header.css";
 export const Header = () => {
   const { addFilmModal } = useSelector((state) => state.main);
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
   const openModalAddHandler = () => {
     dispatch(toggleAddModal(true));
@@ -18,12 +20,12 @@ export const Header = () => {
     <div className="header">
       <div className="container">
         <div className="header-content">
-          <a href="/" className="header-content__link">
+          <Link to="/" className="header-content__link">
             <h1 className="header-content__title">
               <span>BEST</span>films
             </h1>
-          </a>
-          {window.location.pathname === "/" && (
+          </Link>
+          {pathname === "/" && (
             <div className="header-content__button">
               <Button title="Добавить фильм" color="red" onClick={openModalAddHandler}></Button>
             </div>
